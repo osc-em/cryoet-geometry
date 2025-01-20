163c00f1dc70b56af4bcf88a1d4adc26ee3a0514
uermel <uermel@czii.org>
2025-01-20 10:30:05 -0800

    fix some inheritance

--- src/cryoet_metadata/models/image_entities.py
+++ src/cryoet_metadata/models/image_entities.py
@@ -5,49 +5,57 @@ class CTFMetadata(_models.CTFMetadata):
     # Some additional validation logic added here
     pass
 
+
 class AcquisitionMetadataMixin(_models.AcquisitionMetadataMixin):
     # Some additional validation logic added here
     pass
+
 
 class GainFile(_models.Image2D):
     # Some additional validation logic added here
     pass
 
+
 class DefectFile(_models.Image2D):
     # Some additional validation logic added here
     pass
 
-class MovieFrame(_models.Image2D, AcquisitionMetadataMixin):
+
+class MovieFrame(_models.Image2D):
     # Some additional validation logic added here
     pass
 
-class MovieStack(_models._base._models.ImageStack):
+
+class MovieStack(_models.ImageStack):
     # Some additional validation logic added here
     pass
 
-class ProjectionImage(_models.Image2D, AcquisitionMetadataMixin):
+
+class ProjectionImage(_models.Image2D):
     # Some additional validation logic added here
     pass
 
-class MovieStackSeries(_models._base._models.ImageStackSeries):
+
+class MovieStackSeries(_models.ImageStackSeries):
     # Some additional validation logic added here
     pass
 
-class TiltSeries(_models._base._models.ImageStack):
+
+class TiltSeries(_models.ImageStack):
     # Some additional validation logic added here
     pass
+
 
 class SubProjectionImage(_models.ProjectionImage):
     # Some additional validation logic added here
     pass
 
+
 class Tomogram(_models.Image3D):
     # Some additional validation logic added here
     pass
 
+
 class ParticleMap(_models.Image3D):
     # Some additional validation logic added here
     pass
-
-
-
